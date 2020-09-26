8ca0b2a73a19abe497e2557d9bf4da0df4909e89
Slugged <neq.out.1995@gmail.com>
2020-09-26 23:06:44 +0300

    Scaffolded app

--- video-streams/client/src/containers/App/App.js
+++ video-streams/client/src/containers/App/App.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { Route } from "react-router-dom";
+
+import MainPage from "../pages/MainPage";
+import StreamCreate from "../pages/StreamCreate";
+import StreamEdit from "../pages/StreamEdit";
+import StreamShow from "../pages/StreamShow";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Route path="/" exact component={MainPage} />
+      <Route path="/streams/new" component={StreamCreate} />
+      <Route path="/streams/edit" component={StreamEdit} />
+      <Route path="/streams/show" component={StreamShow} />
+    </div>
+  );
 };
 
 export default App;
